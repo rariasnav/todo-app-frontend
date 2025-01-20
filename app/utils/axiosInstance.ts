be96c8bd1abafeb58ca9b8cbe941f42bfdd5d1e9
Rafael Arias <ariasnavarrorafael@gmail.com>
2025-01-20 15:22:23 -0500

    layout modified

--- app/utils/axiosInstance.ts
+++ app/utils/axiosInstance.ts
@@ -3,6 +3,9 @@ import axios from "axios";
 const axiosInstance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
     withCredentials: true,
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+    },
 });
 
 export default axiosInstance;
